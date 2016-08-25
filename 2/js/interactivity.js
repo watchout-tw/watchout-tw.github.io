90989c8ef1067c1d5023897f63e5b0cc40072b07
chihaoyo <chihaoyo@gmail.com>
2016-08-25 21:40:03 +0800

    Add scroll animation

--- 2/js/interactivity.js
+++ 2/js/interactivity.js
@@ -1,5 +1,5 @@
+// timeline event and purchase detail
 $(function() {
-
   var enable_click_focus = function(list_sel, item_sel) {
     var _focus_ = 'focus';
     var $list = $(list_sel);
@@ -15,16 +15,26 @@ $(function() {
   enable_click_focus('#one-time-purchases', '.purchase-option');
 });
 
+// support button
 $(function(){
-  var scrollTarget = $('#about').offset().top - $('nav').outerHeight();
+  var $window = $(window);
+  var redline = $('#about').offset().top - $('nav').outerHeight();
   var $el = $('#support-buttons');
   var _deployed_ = 'deployed';
 
-  $(window).scroll(function(){
-    if($(window).scrollTop() > scrollTarget) {
+  $window.scroll(function(){
+    if($window.scrollTop() > redline) {
       $el.addClass(_deployed_);
     } else {
       $el.removeClass(_deployed_);
     }
   });
+
+  var $html = $('html, body');
+  var $button = $('#support-buttons > .support-button.light');
+  var $scrollTarget = $($button.attr('href'));
+  $button.click(function() {
+    $html.animate({scrollTop: $scrollTarget.offset().top}, 750);
+    return false;
+  })
 });
