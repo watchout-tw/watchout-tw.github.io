8daa260b7d45f64fbc70d1aa6eb173727dbe062b
chihaoyo <chihaoyo@gmail.com>
2016-08-25 16:18:52 +0800

    Fix interactivity; link stickers

--- 2/js/interactivity.js
+++ 2/js/interactivity.js
@@ -1,13 +1,18 @@
 $(function() {
-  var $timeline = $('#timeline');
-  var _focus_ = 'focus';
-  $('#timeline > .event').click(function() {
-    var $event = $(this);
-    if(!$event.hasClass(_focus_)) {
-      $('#timeline > .event').removeClass(_focus_);
-    }
-    $event.toggleClass(_focus_);
-  });
+
+  var enable_click_focus = function(list_sel, item_sel) {
+    var _focus_ = 'focus';
+    var $list = $(list_sel);
+    $(list_sel + ' > ' + item_sel).click(function() {
+      var $item = $(this);
+      if(!$item.hasClass(_focus_)) {
+        $(list_sel + ' > ' + item_sel).removeClass(_focus_);
+      }
+      $item.toggleClass(_focus_);
+    });
+  }
+  enable_click_focus('#timeline', '.event');
+  enable_click_focus('#one-time-purchases', '.purchase-option');
 });
 
 $(function(){
