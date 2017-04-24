170c158ee90e710153bcade248982018c89ea180
chihao <chihaoyo@gmail.com>
2017-04-24 16:41:30 +0800

    Add news-template

---
+++ news-template/src/app.js
@@ -0,0 +1,25 @@
+var app = new Vue({
+  el: '#app',
+  methods: {
+  },
+  data: {
+    sections: [
+      {
+        id: 'musou',
+        name: '國會無雙',
+      },
+      {
+        id: 'maa',
+        name: '草漫',
+      },
+      {
+        id: 'edu',
+        name: '公民學院',
+      },
+      {
+        id: 'vendor',
+        name: '草地攤販',
+      },
+    ],
+  },
+});
