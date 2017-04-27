3b426ea81bb798f1abdf80994cec1f491ec36dc4
chihao <chihaoyo@gmail.com>
2017-04-27 18:11:03 +0800

    Minor update

--- src/common.js
+++ src/common.js
@@ -5,7 +5,7 @@ var CommonData = {
   },
 };
 
-var Channel = function(id, name, width, home) {
+var Channel = function(id, width, home, name) {
   this.id = id;
   this.name = name;
   this.classes = {
@@ -30,25 +30,20 @@ var Channel = function(id, name, width, home) {
 };
 
 CommonData.channels = {
-  park:  new Channel('park', '中央公園', 212, 'https://park.watchout.tw/'),
-  lab:   new Channel('lab', '議題實驗室', 232, 'https://lab.watchout.tw/'),
-  musou: new Channel('musou', '國會無雙', 212, 'https://musou.watchout.tw/'),
-  ask:   new Channel('ask', '給問擂台', 232, 'https://ask.watchout.tw/'),
-  edu:   new Channel('edu', '公民學院', 212, 'https://edu.watchout.tw/'),
-  maa:   new Channel('maa', '草漫', 132, 'https://maa.watchout.tw/'),
-  woo:   new Channel('woo', '沃影音', 154, 'https://woo.watchout.tw/'),
-  ly:    new Channel('ly', '立委履歷', 212, 'https://ly.watchout.tw/'),
-  watchout: new Channel('watchout', '沃草', 274, CommonData.links.home),
+  park:  new Channel('park', 212, 'https://park.watchout.tw/', '中央公園'),
+  lab:   new Channel('lab', 232, 'https://lab.watchout.tw/', '議題實驗室'),
+  musou: new Channel('musou', 212, 'https://musou.watchout.tw/', '國會無雙'),
+  ask:   new Channel('ask', 232, 'https://ask.watchout.tw/', '給問擂台'),
+  edu:   new Channel('edu', 212, 'https://edu.watchout.tw/', '公民學院'),
+  maa:   new Channel('maa', 132, 'https://maa.watchout.tw/', '草漫'),
+  woo:   new Channel('woo', 154, 'https://woo.watchout.tw/', '沃影音'),
+  ly:    new Channel('ly', 212, 'https://ly.watchout.tw/', '立委履歷'),
+  watchout: new Channel('watchout', 274, CommonData.links.home, '沃草'),
 };
 
 Vue.component('nav-bar-simple', {
   props: {
-    channel: {
-      type: Object,
-      validator: function(data) {
-        return true;
-      }
-    },
+    channel: Object,
   },
   template: `
   <nav class="navbar sticky-top navbar-light" :class="channel.classes.bg">
