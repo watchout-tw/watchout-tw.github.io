d17c1ef35c188dc1ef2d7d40c55305c52cb2f3e5
chihao <chihaoyo@gmail.com>
2017-04-27 16:21:59 +0800

    Impact Report page complete

--- impact/src/app.js
+++ impact/src/app.js
@@ -1,8 +1,23 @@
 var app = new Vue({
   el: '#app',
   methods: {
+    paths: function(file) {
+      return {
+        compact: this.common.links.home + 'asset/banners/' + file + '-compact.png',
+        wide: this.common.links.home + 'asset/banners/' + file + '-wide.png',
+      };
+    }
+  },
+  computed: {
   },
   data: {
     common: CommonData,
+    reports: [
+      {
+        year: 2016,
+        link: 'Watchout-Impact-Report-2016.pdf',
+        banner: 'impact-report-2016',
+      },
+    ],
   }
 });
