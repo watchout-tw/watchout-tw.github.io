efd9fb0613469bd6bcf1529e031261991492f6df
joanlyu <h920101005@gmail.com>
2017-09-27 12:26:15 +0800

    Create news 9-ask

--- news-template/src/app.js
+++ news-template/src/app.js
@@ -13,26 +13,26 @@ var app = new Vue({
     },
   },
   data: {
-    permalink: 'https://watchout.tw/news/8-wam',
+    permalink: 'https://watchout.tw/news/9-ask',
     cover: {
       image: 'cover.png',
-      issue: '第八期',
-      date: '2017年8月25日',
+      issue: '第九期',
+      date: '2017年9月28日',
     },
     nav: {
-      title: '917沃草年會',
+      title: '立委給問公民推噓',
       toc: [
         {
-          channel: 'edu',
-          title: '公民學院年會開跑',
+          channel: 'ask',
+          title: '桃園立委給問登場',
         },
         {
-          channel: 'ask',
-          title: '台南立委給問精華',
+          channel: 'edu',
+          title: '全台最大哲普年會',
         },
         {
           channel: 'musou',
-          title: '沃草看世界',
+          title: '工傷。勞權 桃園立委回答嗎',
         },
         {
           channel: 'vendor',
@@ -42,43 +42,17 @@ var app = new Vue({
     },
     sections: [ // 這個陣列包含這一期草報的每個區塊
       {
-        id: 'edu',
-        name: '公民學院',
-        title: '公民學院年會開跑',
-        description: '台港兩地共9個哲普團體參與，台灣最大哲學普及社群盛事，就在9月17日「沃草公民學院年會」！',
-        articles: [
-          {
-            link: 'http://bit.ly/philos917 ', // 連結網址
-            thumbnail: 'edu-1.jpg',
-            title: '參加年會這邊請：',
-            text: [
-              '時間：2017年9月17日（日）',
-              '地點：華山文創園區2F拱廳',
-            ],
-          }
-        ],
-        images: [ // 大圖
-          {
-            title: '康德散步',
-            description: '有位國小老師在課堂上拿出假雞雞和保險套進行性教育，並說明女女男男的性行為，而被「特定團體」控告「猥褻、妨害風化」。這爭議怎麼看？',
-            url: 'walk-1.png',
-            orientation: 'portrait',
-            link: 'https://www.facebook.com/citizenedu/photos/a.861134307356973.1073741836.501740576629683/1038932416243827/?type=1&theater ',
-          },
-        ],
-      },
-      {
         id: 'ask',
         name: '給問擂台',
-        title: '台南立委給問精華', // 區塊標題
-        description: '台南立委現場給問，超過150位公民與數十個公民團體上陣，提問砲火猛烈，也讓立委首度對許多議題公開承諾。', // 區塊文字
+        title: '桃園立委給問登場', // 區塊標題
+        description: '「六都立委給問嗎」第二站：桃園 9/23登場，超過百人質詢立委。針對各種在地議題，陳學聖、鄭運鵬兩位立委，現場承諾與表態，公民直接以推噓扇回應立委。', // 區塊文字
         articles: [ // 連結們
           {
-            link: 'https://youtu.be/QIjTHtIRZ1c', // 連結網址
+            link: 'https://youtu.be/PAC8Cz3Kkxk', // 連結網址
             thumbnail: 'ask-1.jpg', // 連結縮圖
-            title: '台南立委給問精華影片', // 連結標題
+            title: '桃園立委給問直播', // 連結標題
             text: [ // 連結附加文字，分行
-              '剪輯：陳東'
+              '當日活動完整影音'
             ],
           },
           {
@@ -86,31 +60,55 @@ var app = new Vue({
             thumbnail: 'ask-2.jpg', // 連結縮圖
             title: '了解更多立委給問', // 連結標題
             text: [ // 連結附加文字，分行
-              '桃園台中高雄雙北，立委給問Coming soon'
+              '台中高雄雙北，立委給問Coming soon'
             ],
           },
         ]
       },
       {
+        id: 'edu',
+        name: '公民學院',
+        title: '百人華山論哲學',
+        description: '沃草公民學院日前在華山舉辦全台最大的哲學普及年會，台港共九個哲普團體與會主講，在場與會聽眾超過百人。',
+        articles: [
+          {
+            link: 'https://goo.gl/rhTei2', // 連結網址
+            thumbnail: 'edu-1.jpg',
+            title: '百人華山論哲學 沃草年會大成功',
+            text: [
+              '作者：廖昱涵',
+            ],
+          }
+        ],
+      },
+      {
         id: 'musou',
         name: '國會無雙',
-        title: '沃草。觀點', // 區塊標題
-        description: '「媒體該不該有立場呢」？沃草認為，每個媒體都該有自己的立場和議題設定方向。請看「沃草觀點」，瞭解我們對議題的想法。', // 區塊文字
+        title: '桃園立委會答嗎？', // 區塊標題
+        description: '桃園立委給問活動順利結束，但對答如流的立委碰上公民團體詢問勞動議題就安靜了下來。台灣勞工權益還有哪些問題，沃草帶您一同回顧。', // 區塊文字
         articles: [ // 連結們
           {
-            link: 'https://musou.tw/focuses/1378', // 連結網址
+            link: 'https://musou.tw/focuses/1308', // 連結網址
             thumbnail: 'musou-1.jpg', // 連結縮圖
-            title: '觀點／大法官釋憲表態「保障婚姻平權」 立法院還在等風向嗎？', // 連結標題
+            title: '工程會：聘違法勞工是包商責任 林淑芬痛批罔顧工安事故', // 連結標題
             text: [ // 連結附加文字，分行
-              '編輯：王希、劉家丞、洪國鈞',
+              '記者：蕭長展',
             ],
           },
           {
-            link: 'https://musou.tw/focuses/1333', // 連結網址
+            link: 'https://musou.tw/focuses/1331', // 連結網址
             thumbnail: 'musou-2.jpg',
-            title: '觀點／改革到位了嗎？「開放的國會」下一步在哪',
+            title: '教育部漠視學生權益？林淑芬揭馬政府曾發文勞動部「別管建教生」',
             text: [
-              '編輯：洪國鈞',
+              '編輯：蕭長展',
+            ],
+          },
+          {
+            link: 'https://musou.tw/focuses/1197', // 連結網址
+            thumbnail: 'musou-2.jpg',
+            title: '林淑芬：勞保給付對石綿職災「間皮細胞瘤」不公 勞動部承諾修改放寬',
+            text: [
+              '編輯：蕭長展',
             ],
           },
         ]
